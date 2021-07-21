3b501a6a948b0df43d3a1bed8d0ef883388e5f40
aleupons <apahoquei@gmail.com>
2021-07-21 17:30:38 +0200

    La contrassenya no es mostra al llistar usuaris o ensenyar un usuari

--- db/controllers/users.js
+++ db/controllers/users.js
@@ -4,8 +4,21 @@ const { list, read, create, update, deleteData } = require("./generalCrud");
 const model = User;
 const modelName = "usuari";
 
-const listUsers = async () => list(model, modelName);
-const showUser = async (userId) => read(userId, model, modelName);
+const listUsers = async () => {
+  const users = await list(model, modelName);
+  const usersObj = users.map((user) => {
+    const userObj = user.toJSON();
+    delete userObj.password;
+    return userObj;
+  });
+  return usersObj;
+};
+const showUser = async (userId) => {
+  const user = await read(userId, model, modelName);
+  const userObj = user.toJSON();
+  delete userObj.password;
+  return userObj;
+};
 const createUser = async (newUser) => create(newUser, model, modelName);
 const modifyUser = async (userId, modifiedUser) =>
   update(userId, modifiedUser, model, modelName);
