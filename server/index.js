15ec1f3ac7d83121d1ac910b1700b0d601eff6de
aleupons <apahoquei@gmail.com>
2021-07-21 20:05:58 +0200

    Inicialització de la BBDD i server refactoritzada

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const debug = require("debug")("can-mateu:server:main");
 const morgan = require("morgan");
 const cors = require("cors");
-const { app, serverInit } = require("./init");
+
+const { app } = require("./init");
 const usersRoute = require("./routes/users");
 const shoppingCartsRoute = require("./routes/shoppingCarts");
 const productsRoute = require("./routes/products");
@@ -14,24 +15,18 @@ const commentsRoute = require("./routes/comments");
 const basketsRoute = require("./routes/baskets");
 const { error404, generalError } = require("./errors");
 
-const serverStart = () => {
-  serverInit();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-  app.use(morgan("dev"));
-  app.use(cors());
-  app.use(express.json());
+app.use("/users", usersRoute);
+app.use("/shopping-carts", shoppingCartsRoute);
+app.use("/products", productsRoute);
+app.use("/payments", paymentsRoute);
+app.use("/orders", ordersRoute);
+app.use("/coupons", couponsRoute);
+app.use("/comments", commentsRoute);
+app.use("/baskets", basketsRoute);
 
-  app.use("/users", usersRoute);
-  app.use("/shopping-carts", shoppingCartsRoute);
-  app.use("/products", productsRoute);
-  app.use("/payments", paymentsRoute);
-  app.use("/orders", ordersRoute);
-  app.use("/coupons", couponsRoute);
-  app.use("/comments", commentsRoute);
-  app.use("/baskets", basketsRoute);
-
-  app.use(error404);
-  app.use(generalError);
-};
-
-module.exports = serverStart;
+app.use(error404);
+app.use(generalError);
