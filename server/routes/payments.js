aa9f5c8adb04dc966d61310730315d6c02172adf
aleupons <apahoquei@gmail.com>
2021-07-23 21:48:07 +0200

    Endpoints comments, orders, payments en funció de la id de  l'usuari loguejat

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -6,7 +6,7 @@ const {
   showPayment,
   createPayment,
 } = require("../../db/controllers/payments");
-const { validationErrors } = require("../errors");
+const { validationErrors, generateError } = require("../errors");
 const paymentSchema = require("../checkSchemas/paymentSchema");
 const { duplicateKeyError } = require("../errors");
 const { authorization } = require("../authorization");
@@ -22,14 +22,31 @@ router.get("/list", authorization(true), async (req, res, next) => {
   }
 });
 
+router.get("/my-payments", authorization(false), async (req, res, next) => {
+  const { userId } = req;
+  try {
+    const paymentsList = await listPayments();
+    const myPayments = await paymentsList.filter((payment) =>
+      payment.orderId.userId._id.equals(userId)
+    );
+    res.json(myPayments);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get(
   "/payment/:id",
   check("id", "Id incorrecta").isMongoId(),
   validationErrors,
   async (req, res, next) => {
+    const { userId } = req;
     const { id } = req.params;
     try {
       const payment = await showPayment(id);
+      if (!payment.orderId.userId._id.equals(userId)) {
+        throw generateError("Accés denegat", 401);
+      }
       res.json(payment);
     } catch (error) {
       next(error);
@@ -42,8 +59,12 @@ router.post(
   checkSchema(paymentSchema),
   validationErrors,
   async (req, res, next) => {
+    const { userId } = req;
     const payment = req.body;
     try {
+      if (!payment.orderId.userId._id.equals(userId)) {
+        throw generateError("Accés denegat", 401);
+      }
       const newPayment = await createPayment(payment);
       res.status(201).json(newPayment);
     } catch (error) {
